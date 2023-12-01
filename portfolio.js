889b87a3429d1ac11947815915b23e4c2793a219
MithunRaj M <120077786+Mithunlavanyaraj@users.noreply.github.com>
2023-12-01 23:59:41 +0530

    Completed codsoft task no1

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+function Message() {
+    alert("Message received Thank you")
+}
+
+function resume() {
+    window.open("https://drive.google.com/file/d/13lDngf59PZlT1wtM0fxu51pY7N4pNwj9/view?usp=sharing")
+}
